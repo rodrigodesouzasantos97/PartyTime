d9ef7b2796324b754b3ff94c8243f4b6326ce96b
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-03-29 20:48:02 -0300

    Removed password

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -6,7 +6,7 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 async function main() {
   try {
     await mongoose.connect(
-      "mongodb+srv://rodrigo:S8nS4F7ZGpbxkXtr@cluster0.bncfxdf.mongodb.net/?appName=Cluster0",
+      "mongodb+srv://rodrigo:<password>@cluster0.bncfxdf.mongodb.net/?appName=Cluster0",
     );
 
     console.log("Conectado ao banco!");
