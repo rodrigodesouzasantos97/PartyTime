a4e8e3f6bd5b8a013c916b077bc7347f77e69aeb
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-03-30 13:57:07 -0300

    Frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+
+import NavBar from "./components/NavBar";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <NavBar />
+      <Outlet />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/routes/CreateParty.jsx
@@ -0,0 +1,9 @@
+const CreateParty = () => {
+  return (
+    <div>
+      <h1>Create Party</h1>
+    </div>
+  );
+};
+
+export default CreateParty;
