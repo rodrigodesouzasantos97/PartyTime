fa26ecff6fbd0fa0ab3f19540dcbcbfac587610b
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-04-07 16:46:12 -0300

    chore: install dotenv and remove mongodb login

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
+
 const dns = require("dns");
+
+require("dotenv").config();
 
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 async function main() {
   try {
     await mongoose.connect(
-      "mongodb+srv://rodrigo:<password>@cluster0.bncfxdf.mongodb.net/?appName=Cluster0",
+      `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.bncfxdf.mongodb.net/?appName=Cluster0`,
     );
 
     console.log("Conectado ao banco!");
